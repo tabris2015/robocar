c8483d808bf5aee3fa6ed3af3558f6b63aacd28b
Jose Laruta <eduardo.laruta@gmail.com>
2018-10-22 21:54:14 -0400

    refactor for constant time

--- src/joy_teleop.cpp
+++ src/joy_teleop.cpp
@@ -6,97 +6,100 @@
 
 class TeleopRobocar
 {
-public:
-  TeleopRobocar();
+  public:
+    TeleopRobocar();
 
-  ros::Timer timer;
-  
-  ros::NodeHandle nh_;
-  void timerCallback(const ros::TimerEvent& event);
+    ros::Timer timer;
 
-private:
-  void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+    ros::NodeHandle nh_;
+    void timerCallback(const ros::TimerEvent &event);
+    float GetInterval();
 
-  int linear_;      // axis id
-  int brake_;
-  int angular_;
+  private:
+    void joyCallback(const sensor_msgs::Joy::ConstPtr &joy);
 
-  double l_scale_;
-  double a_scale_;
-  double l_offset_;
+    int linear_; // axis id
+    int brake_;
+    int angular_;
 
-  ros::Publisher vel_pub_;
-  ros::Publisher velStamped_pub_;
-  ros::Subscriber joy_sub_;
+    double l_scale_;
+    double a_scale_;
+    double l_offset_;
 
-  // for callbacks
-  geometry_msgs::Twist twist; // simple twist message from joystick
-  geometry_msgs::TwistStamped stamped; // stamped twist for sync
+    int freq_hz_;
 
+    ros::Publisher vel_pub_;
+    ros::Publisher velStamped_pub_;
+    ros::Subscriber joy_sub_;
+
+    // for callbacks
+    geometry_msgs::Twist twist;          // simple twist message from joystick
+    geometry_msgs::TwistStamped stamped; // stamped twist for sync
 };
 
-
-TeleopRobocar::TeleopRobocar():
-  linear_(5),
-  brake_(2),
-  angular_(0)
+TeleopRobocar::TeleopRobocar()
 {
+    nh_.param<int>("freq_hz", freq_hz_, 20);
+    if (freq_hz_ <= 0)
+    {
+        ROS_WARN("Invalid frequency value, default: 20Hz");
+        freq_hz_ = 20;
+    }
 
-  nh_.param<int>("axis_linear", linear_, 5);
-  nh_.param<int>("axis_brake", brake_, 2);
-  nh_.param<double>("scale_linear", l_scale_, -0.5);
-  nh_.param<double>("offset_linear", l_offset_, 0.5);
+    nh_.param<int>("axis_linear", linear_, 5);
+    nh_.param<int>("axis_brake", brake_, 2);
+    nh_.param<double>("scale_linear", l_scale_, -0.5);
+    nh_.param<double>("offset_linear", l_offset_, 0.5);
 
-  
-  nh_.param<int>("axis_angular", angular_, 0);
+    nh_.param<int>("axis_angular", angular_, 0);
 
-  nh_.param<double>("scale_angular", a_scale_, -0.34);
+    nh_.param<double>("scale_angular", a_scale_, -0.34);
 
+    vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+    velStamped_pub_ = nh_.advertise<geometry_msgs::TwistStamped>("/stamped_cmd_vel", 1);
 
-
-  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
-  velStamped_pub_ = nh_.advertise<geometry_msgs::TwistStamped>("/stamped_cmd_vel", 1);
-  
-  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 1, &TeleopRobocar::joyCallback, this);
-
- 
+    joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 1, &TeleopRobocar::joyCallback, this);
 }
 
-void TeleopRobocar::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+float TeleopRobocar::GetInterval()
 {
-
-  twist.angular.z = joy->axes[angular_] * a_scale_;
-
-  double acceleration = l_scale_ * joy->axes[linear_] + l_offset_;
-  ROS_DEBUG("%f", acceleration);
-
-  double reverse = l_scale_ * joy->axes[brake_] + l_offset_;
-  ROS_DEBUG("%f", reverse);
-  
-  twist.linear.x = acceleration - reverse;
-  
-  ROS_DEBUG("%f", twist.linear.x);
-  
-  // vel_pub_.publish(twist);
+    return 1.0 / freq_hz_;
 }
 
-void TeleopRobocar::timerCallback(const ros::TimerEvent& event)
+void TeleopRobocar::joyCallback(const sensor_msgs::Joy::ConstPtr &joy)
 {
-  stamped.twist = twist;
-  stamped.header.stamp = ros::Time::now();
 
-  vel_pub_.publish(twist);
-  velStamped_pub_.publish(stamped);
+    twist.angular.z = joy->axes[angular_] * a_scale_;
+
+    double acceleration = l_scale_ * joy->axes[linear_] + l_offset_;
+    ROS_DEBUG("%f", acceleration);
+
+    double reverse = l_scale_ * joy->axes[brake_] + l_offset_;
+    ROS_DEBUG("%f", reverse);
+
+    twist.linear.x = acceleration - reverse;
+
+    ROS_DEBUG("%f", twist.linear.x);
+
+    // vel_pub_.publish(twist);
 }
 
-int main(int argc, char** argv)
+void TeleopRobocar::timerCallback(const ros::TimerEvent &event)
 {
-  ros::init(argc, argv, "joy_teleop");
-  TeleopRobocar teleop_joy;
-  // 20 hz timer for publishing twist stamped
-  teleop_joy.timer = teleop_joy.nh_.createTimer(ros::Duration(0.05),&TeleopRobocar::timerCallback, &teleop_joy);
-  //teleop_joy.timer = teleop_joy.nh_.createTimer(ros::Duration(0.02),&TeleopRobocar::timerCallback, &teleop_joy);
+    stamped.twist = twist;
+    stamped.header.stamp = ros::Time::now();
 
+    vel_pub_.publish(twist);
+    velStamped_pub_.publish(stamped);
+}
 
-  ros::spin();
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "joy_teleop");
+    TeleopRobocar teleop_joy;
+
+    teleop_joy.timer = teleop_joy.nh_.createTimer(ros::Duration(teleop_joy.GetInterval()),
+                                                    &TeleopRobocar::timerCallback,
+                                                    &teleop_joy);
+    ros::spin();
 }
--- src/laser_node.cpp
+++ src/laser_node.cpp
@@ -12,11 +12,12 @@ class LaserNode
 
     ros::NodeHandle nh_;
     void timerCallback(const ros::TimerEvent &event);
-
+    float GetInterval();
   private:
     // sensor
     VL53L0X sensor_;
 
+    int freq_hz_;
     double l_scale_;
     double a_scale_;
     double l_offset_;
@@ -28,6 +29,13 @@ class LaserNode
 
 LaserNode::LaserNode()
 {
+    nh_.param<int>("freq_hz", freq_hz_, 20);
+    if (freq_hz_ <= 0)
+    {
+        ROS_WARN("Invalid frequency value, default: 20Hz");
+        freq_hz_ = 20;
+    }
+
     // init publisher
     laser_pub_ = nh_.advertise<sensor_msgs::Range>("laser", 1);
     
@@ -41,6 +49,11 @@ LaserNode::LaserNode()
     laser_msg_.field_of_view = 0.1;
     laser_msg_.min_range = 0.0;
     laser_msg_.max_range = 1.20;
+}
+
+float LaserNode::GetInterval()
+{
+    return 1.0 / freq_hz_;
 }
 
 void LaserNode::timerCallback(const ros::TimerEvent &event)
@@ -64,8 +77,9 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "laser_node");
     LaserNode laser_node;
-    // 20 hz timer for publishing twist stamped
-    laser_node.timer = laser_node.nh_.createTimer(ros::Duration(0.05), &LaserNode::timerCallback, &laser_node);
-    //laser_node.timer = laser_node.nh_.createTimer(ros::Duration(0.02),&LaserNode::timerCallback, &laser_node);
+    
+    laser_node.timer = laser_node.nh_.createTimer(ros::Duration(laser_node.GetInterval()), 
+                                                &LaserNode::timerCallback, 
+                                                &laser_node);
     ros::spin();
 }
