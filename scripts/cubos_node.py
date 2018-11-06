86befdcf22ee2c973ca9aca2ce00c5b2a6e44642
Jose Laruta <eduardo.laruta@gmail.com>
2018-11-06 12:45:00 -0400

    guard

--- scripts/cubos_node.py
+++ scripts/cubos_node.py
@@ -276,18 +276,18 @@ class CubosDetector:
         blur = cv2.GaussianBlur(gray,(5,5),0)
         edges = cv2.Canny(blur,50,150,apertureSize = 3)
         lines = cv2.HoughLines(edges,1,np.pi/180,200)
+        if lines:
+            for rho,theta in lines[0]:
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a*rho
+                y0 = b*rho
+                x1 = int(x0 + 1000*(-b))
+                y1 = int(y0 + 1000*(a))
+                x2 = int(x0 - 1000*(-b))
+                y2 = int(y0 - 1000*(a))
 
-        for rho,theta in lines[0]:
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a*rho
-            y0 = b*rho
-            x1 = int(x0 + 1000*(-b))
-            y1 = int(y0 + 1000*(a))
-            x2 = int(x0 - 1000*(-b))
-            y2 = int(y0 - 1000*(a))
-
-            cv2.line(img2,(x1,y1),(x2,y2),(0,0,255),2)
+                cv2.line(img2,(x1,y1),(x2,y2),(0,0,255),2)
 
         # ret,thresh = cv2.threshold(blur,60,255,cv2.THRESH_BINARY_INV)
         # im3,contours,hierarchy = cv2.findContours(thresh.copy(), 1, cv2.CHAIN_APPROX_NONE)
